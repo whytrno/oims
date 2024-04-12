4328c623844cd0b119fca844c5e2b450b1c67b42
Wahyu Triono <whytrno@gmail.com>
2024-04-12 12:50:38 +0700

    Fix: Seeder not called

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // $this->call(RoleSeeder::class);
+        $this->call(RoleSeeder::class);
     }
 }
