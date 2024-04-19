4c6cc7d69b09953767c668e5db95b08e9bf6e57c
Wahyu Triono <whytrno@gmail.com>
2024-04-20 03:04:16 +0700

    Fix: install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,1 @@
-sudo chmod -R ugo+rw storage
-&& composer install 
-&& npm install
-&& npm run build
-&& cp .env.example .env
-&& php artisan key:generate
-&& php artisan migrate --seed
-&& php artisan storage:link
+sudo chmod -R ugo+rw storage && composer install  && npm install && npm run build && cp .env.example .env && php artisan key:generate && php artisan migrate --seed && php artisan storage:link
